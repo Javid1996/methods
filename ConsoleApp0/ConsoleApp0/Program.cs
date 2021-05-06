26bd32242b2b4137676078c99ca83f5da86b5b62
Javid1996 <alcavid96@gmail.com>
2021-05-07 03:38:21 +0400

    lanchester

---
+++ ConsoleApp0/ConsoleApp0/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace ConsoleApp0
+{
+    class Point
+    {
+        private int x;
+        private int y;
+
+        public Point(int x=0 ,int y=0 )
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        
+        public void Displayinfo()
+        {
+            Console.WriteLine($"x = {x}, y = {y}");
+        }
+
+        public double Distance()
+        {
+            
+            return Math.Sqrt(x * x) + (y * y);
+
+        }
+
+        public void Displacement(int a , int b) 
+        {
+           
+            x += a;
+            y += b;
+        }
+
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+
+        public int scalX 
+        {
+            set 
+            {
+                x *= value; 
+            }
+        }
+
+        public int scalY
+        {
+            set
+            {
+                y *= value;
+            }
+        }
+
+
+       
+
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        { int a = AddVector();
+            int b = AddVector();
+            int x=Addpoint();
+            int y=Addpoint();
+            Point p = new Point(x,y);
+            Console.WriteLine("вывести координаты точки на экран:");
+            p.Displayinfo();
+            Console.Write("расстояние от начала координат до точки:");
+            p.Distance();
+            p.Displacement();
+            
+            
+
+            Console.ReadKey();
+
+
+        }
+            static int Addpoint()
+        {
+            Console.WriteLine("Введите координаты точки ");
+     
+            for(; ; )
+            {
+                int temp;
+                string message = Console.ReadLine();
+            if(Int32.TryParse(message, out temp))
+                {
+                    return temp;
+                }
+                else
+                {
+                    Console.WriteLine("Введите корректные данные");
+                }
+            }
+       
+        }
+
+        static int AddVector()
+        {
+            Console.WriteLine("Введите координаты вектора ");
+
+            for (; ; )
+            {
+                int temp;
+                string message = Console.ReadLine();
+                if (Int32.TryParse(message, out temp))
+                {
+                    return temp;
+                }
+                else
+                {
+                    Console.WriteLine("Введите корректные данные");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Array 
+    {
+        private int n;
+        private int m;
+        private double[,] DoubleArray;
+
+        public Array(int n, int m)
+        {
+            this.n = n;
+            this.m = m;
+            DoubleArray = new double[n,m];
+        }
+
+
+        public int Arraylength
+        {
+            get { return n*m; }
+            
+        }
+
+        public double Scalar
+        {
+            
+            set {
+                
+                     for (int i = 0; i <n; i++)
+                     {
+                        for(int j=0; j< m; j++)
+                        {
+                            DoubleArray[i, j] += value;
+                        }
+                     }
+                }
+        }
+
+        public void AddElements()
+        {
+            for (int i=0;i<n;i++)
+            {
+                for(int j=0; j < m; j++)
+                {
+                    Console.WriteLine($"Введите элемент массива { i},{j}");
+                    DoubleArray[i, j] = Test2();
+                }
+            }
+        }
+
+        public void Displayinfo()
+        {
+            Console.WriteLine("Вывод массива");
+            for(int i=0; i<n; i++)
+            {
+                for (int j = 0; j < m; j++) 
+                {
+                    Console.Write(DoubleArray[i, j]+" ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Decreasingsort()
+        {
+            double[] temparray = new double[n * m];
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    temparray[m * i + j] = DoubleArray[i, j];
+                }
+            }
+            double temp;
+            
+            
+            for(int i=0; i < n*m; i++)
+            {
+                for(int j = i + 1; j < n*m; j++)
+                {
+                    if (temparray[i] < temparray[j])
+                    {
+                        temp = temparray[i];
+                        temparray[i] = temparray[j];
+                        temparray[j] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    DoubleArray[i, j]=temparray[m * i + j] ;
+                }
+            }
+        }
+
+        static double Test2()
+        {
+            for (; ; )
+            {
+                double t;
+                Console.WriteLine("введите значение  массива");
+                string message = Console.ReadLine();
+                if (double.TryParse(message, out t))
+                {
+
+                    return t;
+                }
+                else
+                {
+                    Console.WriteLine("Введите правильное значение");
+                }
+            }
+        }
+    }
+
+     
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Test();
+            int m = Test();
+           
+            Array M = new Array( n, m);
+            M.AddElements();
+            M.Displayinfo();
+            M.Decreasingsort();
+            M.Displayinfo();
+            Console.WriteLine("\nArray length is :" + M.Arraylength);
+            M.Scalar = 3;
+
+            M.Displayinfo();
+            Console.ReadKey();
+        }
+
+        static int Test()
+        {
+            for (; ; )
+            {
+                int t;
+                Console.WriteLine("введите длину массива");
+                string message = Console.ReadLine();
+                if (Int32.TryParse(message, out t))
+                {
+
+                    return t;
+                }
+                else
+                {
+                    Console.WriteLine("Введите правильное значение");
+                }
+            }
+        }
+
+        
+    }
+
+}
